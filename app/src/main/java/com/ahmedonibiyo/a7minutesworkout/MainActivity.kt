971f9050b12df2b478d8377cce815d85ec6ad488
Ahmed021 <ahmedabiodun4@gmail.com>
2022-04-07 23:42:03 +0100

    Initial commit

---
+++ app/src/main/java/com/ahmedonibiyo/a7minutesworkout/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.ahmedonibiyo.a7minutesworkout
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.ahmedonibiyo.a7minutesworkout.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private var binding: ActivityMainBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        binding?.flStart?.setOnClickListener {
+            val intent = Intent(this, ExerciseActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
+    }
+}
